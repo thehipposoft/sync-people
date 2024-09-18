b0ee9c6fbbbce673a330c37b5b1d45b4e43098a4
Diego Borigen <diego@iddigital.com.au>
2024-09-18 14:00:18 +1000

    Fix build issue

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Header from '../../../components/Header'
-import Welcome from '../../../components/Welcome'
+//import Welcome from '../../../components/Welcome'
 
 export default async function ProfilePage() {
     return(
         <div className="h-screen overflow-hidden">
             <Header />
-            <Welcome />
         </div>
     )
 }
