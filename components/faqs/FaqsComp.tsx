f49c41593c5f29530d266a12eabeb3657223808f
Juan Manuel Borigen <juanmanuelborigen@MBP-de-Juan.lan>
2025-06-12 09:44:17 +0200

    fixing deploy

--- components/faqs/FaqsComp.tsx
+++ components/faqs/FaqsComp.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import Accordion from './Accordion';
+import FaqAccordion from './Accordion';
 
 type Props = {
     faqsData: any;
@@ -13,7 +13,7 @@ const FaqsComp = ({faqsData}:Props) => {
         <div className='flex flex-col gap-12 md:gap-0 md:flex-row items-center md:items-start md:w-[1250px] w-[85vw] py-12 mx-auto justify-between'>
             <div className='flex flex-col gap-12'>
                 <h1>FAQs</h1>
-                <Accordion items={faqsData} />
+                <FaqAccordion items={faqsData} />
             </div>
             <div className='relative md:w-[450px] w-[85vw] h-[525px] '>
                 <div className='absolute left-0 z-10 h-full w-full rounded-tl-[250px] bg-gradient-to-l opacity-30 rounded-br-[250px] from-[#8D78E0] to-[#15DFBB] ' />
