9a6e1636c9a314339557947f25b6eb054b4618bb
Juan Manuel Borigen <juanmanuelborigen@MBP-de-Juan.lan>
2025-07-01 10:42:35 +0200

    Correcting download button hidded on desktop

--- components/DownloadButton.tsx
+++ components/DownloadButton.tsx
@@ -37,7 +37,7 @@ export default function InstallButton() {
   return (
     <button
       onClick={handleInstallClick}
-      className='hidden md:block uppercase fixed bottom-5 right-5 py-3 px-5 bg-[#FF8149] border border-[#FF8149] duration-300 hover:text-[#FF8149] text-white z-50 text-sm rounded-3xl hover:bg-transparent'
+      className='block md:hidden uppercase fixed bottom-5 right-5 py-3 px-5 bg-[#FF8149] border border-[#FF8149] duration-300 hover:text-[#FF8149] text-white z-50 text-sm rounded-3xl hover:bg-transparent'
     >
       download
     </button>
